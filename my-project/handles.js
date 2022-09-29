e65318636f1985d5f679d9915cb50aa5500598ed
RXT-Eago <clement.delbarre@edu.ece.fr>
2022-09-29 20:05:45 +0200

    Partie 6: JSON file to HTML

--- my-project/handles.js
+++ my-project/handles.js
@@ -1,7 +1,19 @@
 const http = require('http')
 const url = require('url')
 const qs = require('querystring')
+const JSONContent = require('./content/about.json')
 
+const content = '<!DOCTYPE html>' +
+'<html>' +
+'    <head>' +
+'        <meta charset="utf-8" />' +
+'        <title>Martin Bio</title>' +
+'    </head>' + 
+'    <body>' +
+'       <p>Bio de Martin: </p>' +
+'       <p>Coucou je suis un étudiant de l\'ECE ancien Top 1 Promo (crack)</p>' +
+'    </body>' +
+'</html>'
 
 module.exports = {
     serverHandle: function (req, res) {
@@ -14,10 +26,21 @@ module.exports = {
 
         res.writeHead(200, { 'Content-Type': 'text/html' });
 
-        if (path === '/hello' && 'name' in params) {
-            res.write('Hello ' + params['name'])
-        } else {
-            res.write('Hello anonymous')
+        if (path === '/') {
+            res.write('if you want to know more about Martin type in the url /hello&name=martin else you can just replace it by your name, DON\'T forgot the url: /about');
+        } else if (path === '/hello' && 'name' in params){
+            if (params['name'] === "martin")
+                res.write(content);
+            else
+                res.write('Hello ' + params['name']);
+        }else if(path === '/about'){
+            res.setHeader('Content-Type', 'application/json');            res.end(JSON.stringify(JSONContent));
+            
+            //res.write(JSONContent);
+            console.log(JSONContent);
+        }
+        else {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
         }
 
         res.end();
