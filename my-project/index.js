192f83cf3d63ab7aa02b7ee76ae6d93ab41f1724
RXT-Eago <clement.delbarre@edu.ece.fr>
2022-10-05 12:16:09 +0200

    Pour tinmar

--- my-project/index.js
+++ my-project/index.js
@@ -1,4 +1,11 @@
-const http = require('http')
-const handles = require('./handles')
-const server = http.createServer(handles.serverHandle);
-server.listen(8080)
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
